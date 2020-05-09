8cd4ce4828ea33d4cf9272b65942808bcf2c06fd
Johnny Perdomo <johnnyperdomo7@gmail.com>
2020-05-09 14:12:11 -0400

    added payment dashboard stat card skeletons, view payouts button, added css dashkit libs to angular.json

--- src/app/dashboard/payments/payments.component.ts
+++ src/app/dashboard/payments/payments.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-payments',
   templateUrl: './payments.component.html',
-  styleUrls: ['./payments.component.scss']
+  styleUrls: ['./payments.component.scss'],
 })
 export class PaymentsComponent implements OnInit {
+  payments = '$30'; //pseudo code
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
