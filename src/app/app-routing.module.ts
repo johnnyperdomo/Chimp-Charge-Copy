443ef605473cdd88572f24ab4ced8452adbbc4f2
Johnny Perdomo <johnnyperdomo7@gmail.com>
2020-05-08 21:04:00 -0400

    added dashboard components, navigation items, link routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppComponent } from './app.component';
+import { PaymentsComponent } from './dashboard/payments/payments.component';
+import { CustomersComponent } from './dashboard/customers/customers.component';
+import { SubscribersComponent } from './dashboard/subscribers/subscribers.component';
+import { PlansComponent } from './dashboard/plans/plans.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/payments', pathMatch: 'full' },
+  { path: 'payments', component: PaymentsComponent },
+  { path: 'customers', component: CustomersComponent },
+  { path: 'subscribers', component: SubscribersComponent },
+  { path: 'plans', component: PlansComponent },
+  { path: '**', redirectTo: '/payments' }, //TODO : make this a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import { PaymentsComponent } from './dashboard/payments/payments.component';
+import { CustomersComponent } from './dashboard/customers/customers.component';
+import { SubscribersComponent } from './dashboard/subscribers/subscribers.component';
+import { PlansComponent } from './dashboard/plans/plans.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent, HeaderComponent, PaymentsComponent, CustomersComponent, SubscribersComponent, PlansComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
