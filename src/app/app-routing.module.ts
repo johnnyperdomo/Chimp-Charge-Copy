ce480b05e4b9657a73353aef20520924833502e3
Johnny Perdomo <johnnyperdomo7@gmail.com>
2020-05-09 08:06:26 -0400

    added settings dropdown, child components, and child routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AppComponent } from './app.component';
 import { PaymentsComponent } from './dashboard/payments/payments.component';
 import { CustomersComponent } from './dashboard/customers/customers.component';
 import { SubscribersComponent } from './dashboard/subscribers/subscribers.component';
 import { PlansComponent } from './dashboard/plans/plans.component';
+import { SettingsComponent } from './dashboard/settings/settings.component';
+import { AccountComponent } from './dashboard/settings/account/account.component';
+import { BillingComponent } from './dashboard/settings/billing/billing.component';
+import { PayoutsComponent } from './dashboard/settings/payouts/payouts.component';
 
 const routes: Routes = [
+  //Main Navs
   { path: '', redirectTo: '/payments', pathMatch: 'full' },
   { path: 'payments', component: PaymentsComponent },
   { path: 'customers', component: CustomersComponent },
   { path: 'subscribers', component: SubscribersComponent },
   { path: 'plans', component: PlansComponent },
+
+  //Settings
+  { path: 'settings', redirectTo: 'settings/account' }, //redirects to settings/account
+  {
+    path: 'settings',
+
+    component: SettingsComponent,
+    children: [
+      { path: 'account', component: AccountComponent },
+      { path: 'billing', component: BillingComponent },
+      { path: 'payouts', component: PayoutsComponent },
+    ],
+  },
+
+  //404 - Page not found
   { path: '**', redirectTo: '/payments' }, //TODO : make this a 404 page
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,13 @@ import { PaymentsComponent } from './dashboard/payments/payments.component';
 import { CustomersComponent } from './dashboard/customers/customers.component';
 import { SubscribersComponent } from './dashboard/subscribers/subscribers.component';
 import { PlansComponent } from './dashboard/plans/plans.component';
+import { SettingsComponent } from './dashboard/settings/settings.component';
+import { AccountComponent } from './dashboard/settings/account/account.component';
+import { BillingComponent } from './dashboard/settings/billing/billing.component';
+import { PayoutsComponent } from './dashboard/settings/payouts/payouts.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, HeaderComponent, PaymentsComponent, CustomersComponent, SubscribersComponent, PlansComponent],
+  declarations: [AppComponent, DashboardComponent, HeaderComponent, PaymentsComponent, CustomersComponent, SubscribersComponent, PlansComponent, SettingsComponent, AccountComponent, BillingComponent, PayoutsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
