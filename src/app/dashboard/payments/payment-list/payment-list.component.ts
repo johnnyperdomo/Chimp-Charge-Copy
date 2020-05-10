69512f74f36065674783baf0935a4ac275909c9b
Johnny Perdomo <johnnyperdomo7@gmail.com>
2020-05-09 20:23:52 -0400

    added email to payment model, added email to payment table row, added bold styling to payment amount in payment table row

--- src/app/dashboard/payments/payment-list/payment-list.component.ts
+++ src/app/dashboard/payments/payment-list/payment-list.component.ts
@@ -8,10 +8,42 @@ import { Payment } from '../payment.model';
 })
 export class PaymentListComponent implements OnInit {
   payments: Payment[] = [
-    new Payment('1', 'Johnny J', '30', 'Success', '11/1/2020', 'Single'),
-    new Payment('2', 'Bobby B', '50', 'Dispute', '11/18/2020', 'Recurring'),
-    new Payment('3', 'Peter T', '80', 'Refunded', '11/19/2020', 'Single'),
-    new Payment('4', 'Lenny L', '24', 'Success', '11/20/2020', 'Recurring'),
+    new Payment(
+      '1',
+      'Johnny J',
+      'johnny@jim.com',
+      '30.84',
+      'Success',
+      '11/1/2020',
+      'Single'
+    ),
+    new Payment(
+      '2',
+      'Bobby B',
+      'bobby@bob.com',
+      '50',
+      'Dispute',
+      '11/18/2020',
+      'Recurring'
+    ),
+    new Payment(
+      '3',
+      'Peter T',
+      'peter@thiel.com',
+      '80',
+      'Refunded',
+      '11/19/2020',
+      'Single'
+    ),
+    new Payment(
+      '4',
+      'Lenny L',
+      'lenny@lion.com',
+      '24.57',
+      'Success',
+      '11/20/2020',
+      'Recurring'
+    ),
   ];
 
   constructor() {}
--- src/app/dashboard/payments/payment.model.ts
+++ src/app/dashboard/payments/payment.model.ts
@@ -2,6 +2,7 @@
 export class Payment {
   public paymentID: string;
   public customerName: string;
+  public customerEmail: string;
   public amount: string;
   public status: string;
   public date: string;
@@ -10,6 +11,7 @@ export class Payment {
   constructor(
     paymentID: string,
     customerName: string,
+    customerEmail: string,
     amount: string,
     status: string,
     date: string,
@@ -17,6 +19,7 @@ export class Payment {
   ) {
     this.paymentID = paymentID;
     this.customerName = customerName;
+    this.customerEmail = customerEmail;
     this.amount = amount;
     this.status = status;
     this.date = date;
