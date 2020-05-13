09a8d85f9bf101b90d7678dd4bd072f357a67133
Johnny Perdomo <johnnyperdomo7@gmail.com>
2020-05-13 19:28:52 -0400

    add firebase environment keys to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCikXqOWu98MqEjplpXth4HatoECoXiZmY',
+    authDomain: 'chimp-charge.firebaseapp.com',
+    databaseURL: 'https://chimp-charge.firebaseio.com',
+    projectId: 'chimp-charge',
+    storageBucket: 'chimp-charge.appspot.com',
+    messagingSenderId: '273131846932',
+  },
 };
