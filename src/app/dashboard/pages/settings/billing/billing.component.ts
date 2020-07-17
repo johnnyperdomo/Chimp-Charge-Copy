98c499f78a1390d1c490c959a9fd73d12ea80629
Johnny Perdomo <johnnyperdomo7@gmail.com>
2020-07-17 09:22:15 -0400

    add loading spinner to billing portal session creation on settings page

--- src/app/dashboard/pages/settings/billing/billing.component.ts
+++ src/app/dashboard/pages/settings/billing/billing.component.ts
@@ -7,21 +7,27 @@ import { HelperService } from 'src/app/shared/helper.service';
   styleUrls: ['./billing.component.scss'],
 })
 export class BillingComponent implements OnInit {
+  isBillingPortalLoading = false;
+
   constructor(private helperService: HelperService) {}
 
   ngOnInit(): void {}
 
   async onCreateBillingPortalSession() {
-    //LATER: add loading spinner and disable button when clicked to prevent spamming.
+    //LATER: disable button when clicked to prevent spamming.
     //LATER: instead of having billing portal, just have all the billing info on this page.
     try {
+      this.isBillingPortalLoading = true;
+
       const portalSession: any = await this.helperService.createBillingPortalSession();
       const portalURL = portalSession.url;
 
       window.open(portalURL);
 
+      this.isBillingPortalLoading = false;
       return portalSession;
     } catch (err) {
+      this.isBillingPortalLoading = false;
       alert(
         'Problem connecting to stripe, please try again. Error: ' + err.error
       );
