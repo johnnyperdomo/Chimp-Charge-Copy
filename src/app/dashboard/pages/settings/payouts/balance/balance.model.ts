bda81904a148e9796802997c8d1f9d088e486cb4
Johnny Perdomo <johnnyperdomo7@gmail.com>
2020-08-25 09:14:00 -0400

    remove balance available object from stripe api call

--- src/app/dashboard/pages/settings/payouts/balance/balance.model.ts
+++ src/app/dashboard/pages/settings/payouts/balance/balance.model.ts
@@ -8,20 +8,6 @@ export class Balance {
     this.balance = balance;
   }
 
-  //LATER: get list of all payouts, and then only get the 'in_transit' status, that is 'on the way to bank' method.
-
-  // on the way to bank
-  get availableAmount() {
-    //i.e. $350.00
-
-    //get sum of all available amounts in array
-    const totalAvailable = this.balance.available
-      .map((arr) => arr.amount)
-      .reduce((a, b) => a + b, 0);
-
-    return MoneyFormatter.convertMinorUnitToStandard(totalAvailable);
-  }
-
   // estimated future payouts; subject rolling day cycles
   get pendingAmount() {
     //i.e. $350.00
@@ -35,10 +21,7 @@ export class Balance {
   }
 
   get totalAmount() {
-    const total = MoneyFormatter.getSumOfNumbers([
-      this.availableAmount,
-      this.pendingAmount,
-    ]);
+    const total = MoneyFormatter.getSumOfNumbers([this.pendingAmount]);
 
     return MoneyFormatter.convertMinorUnitToStandard(total);
   }
